5f6cb390c270796290a529fa02ad99b2c62674b3
pakhtusov_k <pak.kiruha@yandex.ru>
2023-08-29 19:06:46 +0300

    add printf
    add printf

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("good");
+        System.out.println("good2");
+        System.out.println("good2");
         PhoneBook book = new PhoneBook();
         book.init();
         book.run();
